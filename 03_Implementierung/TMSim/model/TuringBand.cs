8e60ea7187f39f188a4bfd34c84a8b25c8817e18
Marcus Haberl <m.haberl@oth-aw.de>
2022-04-24 16:22:33 +0000

    Dokumentation merge into develop

---
+++ 03_Implementierung/TMSim/model/TuringBand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMSim.model
+{
+    class TuringBand
+    {
+        public string Content { get; set; }
+
+        public int HeadIndex { get; private set; }
+        public char GetCurrentSymbol()
+        {
+            throw new NotImplementedException("lulz");
+        }
+
+        public void MoveLeft()
+        {
+
+        }
+
+        public void MoveRight()
+        {
+
+        }
+    }
+}
