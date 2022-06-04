3e8d59a007f0d66f702cc21343295a1379553679
Thomas Götzl <t.goetzl@oth-aw.de>
2022-06-04 09:10:23 +0000

    Singleview und Multiview ueberarbeitet

---
+++ 03_Implementierung/TMSim.UI/Converter/BoolToColumnWidthConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TMSim.UI
+{
+    public class BoolToColumnWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool && (bool)value == false)
+            {
+                return new GridLength(0); //Unsichtbar
+            }
+
+            return new GridLength(1, GridUnitType.Star); //Sichtbar            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new ArgumentException();
+        }
+    }
+}
