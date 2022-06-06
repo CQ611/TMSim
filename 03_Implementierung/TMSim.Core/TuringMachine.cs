abade0fcc7c6169337975ad6b08680e0088badbd
Liam Schäfer <liam.schaefer@oth-aw.de>
2022-06-06 07:32:08 +0200

    Adjusted tests for new edit function

--- 03_Implementierung/TMSim.Core/TuringMachine.cs
+++ 03_Implementierung/TMSim.Core/TuringMachine.cs
@@ -154,6 +154,7 @@ namespace TMSim.Core
 
         public void EditState(TuringState tsOld, TuringState tsNew)
         {
+            foreach (TuringState state in States) if (state.Identifier == tsNew.Identifier) throw new StateAlreadyExistsException();
             tsOld.Identifier = tsNew.Identifier;
             tsOld.Comment = tsNew.Comment;
 
